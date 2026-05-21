857d4a8e43dafcc416e49a5f07530b0618f938ce
Aykrifa <aykrifa@student.42.fr>
2026-05-21 16:58:37 +0200

    Pretty bold/ highlights now psuh into holding vector is right after insertion seem to be the same shit still need to fix acces before vector

--- ex02/classes/PmergeMe/PmergeMe.cpp
+++ ex02/classes/PmergeMe/PmergeMe.cpp
@@ -51,13 +51,13 @@ countComp
 	return a < b;
 }
 
-// Put greaters elements of the main chain after smallers
+// INFO: Put greaters elements of the main chain after smallers
 void
 sort2By2
 (std::vector<int> &main, std::vector<int>::size_type sizeOfElement)
 {
 	int	step = sizeOfElement * 2;
-	// for each pair of block of number (element)
+	// INFO: for each pair of block of number (element)
 	for (std::vector<int>::iterator secondRangeElementToCompare = main.begin() + step - 1;
 	secondRangeElementToCompare < main.end();
 	secondRangeElementToCompare += step)
@@ -66,70 +66,69 @@ sort2By2
 		std::vector<int>::iterator	firstRangeEnd = firstRangeBegin + sizeOfElement;
 		std::vector<int>::iterator	firstRangeElementToCompare = firstRangeEnd - 1;
 		std::vector<int>::iterator	secondRangeBegin = firstRangeEnd;
-		// Compare last number of each elements
+		// INFO: Compare last number of each elements
 		if (countComp(*secondRangeElementToCompare, *firstRangeElementToCompare))
-			// Put smaller before larger
+			// INFO: Put smaller before larger
 			std::swap_ranges(firstRangeBegin, firstRangeEnd, secondRangeBegin);
 	}
 }
 
-// append pending elements that lost against greaters in pend chain
-// trim them from main chain by putting it in newmain chain that is swapped
-// so i need to put the first element in the main / i don't need to call this function if there is less than 3 element
+// INFO: append pending elements that lost against greaters in pend chain
+// PERF: trim them from main chain by putting it in newmain chain that is swapped
 void
 extractPend
 (std::vector<int> &main, std::vector<int> &pend, int sizeOfElement)
 {
 	std::vector<int> newMain;
-	// alloc main size in vector
+	// PERF: alloc main size in vector
 	newMain.reserve(main.size() + sizeOfElement);
-	// new main chain is composed by first element + odd element
+	// INFO: new main chain is composed by first element + odd element
 	newMain.insert(newMain.end(), main.begin(), main.begin() + 2 * sizeOfElement);
 
 	pend.reserve(main.size() / 2);
 
 	bool	even = true;
 
-	// for each pair of block of number (element)
+	// INFO :for each pair of block of number (element)
 	for (std::vector<int>::iterator rangeBegin = main.begin() + 2 * sizeOfElement; rangeBegin != main.end(); rangeBegin += sizeOfElement)
 	{
 		std::vector<int>::iterator rangeEnd = rangeBegin + sizeOfElement;
 
-		// pending elements are even elements except first one
+		// INFO: pending elements are even elements except first one
 		if (even)
 			pend.insert(pend.end(), rangeBegin, rangeEnd);
 
-		// new main chain is composed by first element + odd element
+		// INFO: new main chain is composed by first element + odd element
 		else
 			newMain.insert(newMain.end(), rangeBegin, rangeEnd);
 
 		even = !even;
 	}
+	// PERF: swap ownership of mainchain
 	main.swap(newMain);
 }
 
-//binary insertion
-// BORNE EXCLUSIF ?? (+1)
 int
 placeNumber
 (std::vector<int> &main, std::vector<int> &pend, int nbToPlaceId, int borne, int sizeOfElement)
 {
+	// INFO: Info de l'element a inserer
 	std::vector<int>::iterator toInsertBegin = pend.begin() + nbToPlaceId - sizeOfElement + 1;
 	std::vector<int>::iterator toInsertEnd = pend.begin() + nbToPlaceId + 1;
-
-	std::cout << "id :" << nbToPlaceId << std::endl;
-	std::cout << "pend" << ": " << printContainer(pend, sizeOfElement) << std::endl;
 	int	toCompare = pend[nbToPlaceId];
-	// me
-	// orphan has no winner pair: borne may exceed main.size(), clamp to end
-	std::vector<int>::iterator searchEnd = main.begin() + borne + 1;
-	if (searchEnd > main.end())
-		searchEnd = main.end();
-	std::vector<int>::iterator place = StrideLowerBound(main.begin(), searchEnd, toCompare, sizeOfElement, countComp);
-	// error
-	main.insert(place, toInsertBegin, toInsertEnd);
-	// error
 
+	// INFO: Index de la borne dans le main
+	// WARN: ne gere pas le non paire
+	// TODO: Borner en amont a end le non paire
+	std::vector<int>::iterator searchEnd = main.begin() + borne + 1;
+
+	// INFO: Binary search tout les sizeOfElement le lower bound de notre element a inserer
+	std::vector<int>::iterator place = StrideLowerBound(main.begin(), searchEnd, toCompare, sizeOfElement, countComp);
+
+	// PERF: insertion range
+	main.insert(place, toInsertBegin, toInsertEnd); // BUG: cause des acces avant le debut du vector
+
+	// INFO: index a considerer pour trouver les prochaines bornes a cause du decalage
 	std::vector<int>::difference_type i = place - main.begin();
 	return i;
 }
@@ -138,57 +137,60 @@ void
 insertPend
 (std::vector<int> &main, std::vector<int> pend, int sizeOfElement)
 {
-	std::vector<int> insertedSinceLastJacobsthal;
-	int lastInsertedMain = 0;
+	std::vector<int> insertionIndexesSinceLastJacobsthal;
 
-	// cree l'ordre dans lequel les pends vont etre inseres
+	// INFO: cree l'ordre dans lequel les pends vont etre inseres
 	std::vector<int> insertionOrder = createinsertionOrder<std::vector<int> >(pend.size() / sizeOfElement);
 
 	std::cout << "size of jacob:" << insertionOrder.size() << std::endl;
 
-	// self explanatory
-	// sera utile pour savoir si on est passe d'un jacob a un autre dans la liste ou si on a decremente entre deux
+	// INFO: sera utile pour savoir si on est passe d'un jacob a un autre dans la liste
+	// ou si on a decremente entre deux
 	int prevInsertId = 0;
 
-	// valeur de decalage des gagnants de base 
-	// compte tenu du fait qu'il ya un perdant au debut
+	// INFO: valeur de decalage initial pour trouver les bornes
+	// compte tenu du fait qu'il ya un element perdant au debut de la main chain
 	int mainShift = sizeOfElement;
 
-	// pour chaque pend dans la liste de jacob
+	// INFO: pour chaque pend dans la liste de jacob
+	// WARN: ne gere plus l'element non paire
+	// TODO: il faut determiner sa borne dans la for
+	// sans le calculer a chaque iteration
 	for (std::vector<int>::iterator toInsertId = insertionOrder.begin(); toInsertId != insertionOrder.end(); ++toInsertId)
 	{
-		// index of the beggining of the element
+		// INFO: index of the element
 		int pendToInsertId = *toInsertId * sizeOfElement;
-		// between 2 jacob
+
+		// INFO: between 2 jacob
 		if (prevInsertId > *toInsertId)
 		{
-			// ajoute la paire precedente a holding
-			insertedSinceLastJacobsthal.push_back(lastInsertedMain);
-			// trouve la paire sans le decalge de holding
+			// INFO: trouve la paire sans le decalge de holding
 			int mainBoundInsertion = pendToInsertId + mainShift;
-			// decale en fonction de holding
-			considerPreviousInsertion(insertedSinceLastJacobsthal, mainBoundInsertion, sizeOfElement);
+			// INFO: decale en fonction des insertions precedente pour tomber sur la borne exactement
+			considerPreviousInsertion(insertionIndexesSinceLastJacobsthal, mainBoundInsertion, sizeOfElement);
 	
-			// insere et edite lastInsertedMain a l'insertionm actuelle
-			lastInsertedMain = placeNumber(main, pend, pendToInsertId, mainBoundInsertion, sizeOfElement);
+			// INFO: insere et edite lastInsertedMain a l'insertion actuelle
+			// puis ajoute la position dans le main de l'insertion precedente
+			int insertionIndex = placeNumber(main, pend, pendToInsertId, mainBoundInsertion, sizeOfElement);
+			insertionIndexesSinceLastJacobsthal.push_back(insertionIndex);
 		}
-		// new jacob
+		// INFO: new jacob
 		else
 		{
-			// decale de taille de holding * sizeofelement
-			// Pareil que de decaler de jacob actu - precedent jacob 
-			// mais vu que la liste a ete generee avec les gradients decremente,
-			// oblige de faire avec la taille de holding qui est egale au nombre d'iteration de l'autre if
-			// et donc on doit push back holding au debut du if
-			// (alors qu ele plus logique c'est apres linsertion pour arreter de trimballer lastInsertedMain dans le code)
-			mainShift += insertedSinceLastJacobsthal.size() * sizeOfElement;
-			insertedSinceLastJacobsthal.clear();
+			// INFO: les insertions precedentes sont forcement avant le nouveau jacob
+			// donc decale de taille de previous insertion * sizeofelement (=nombres inseres avant la nouvelle borne)
+			// puis flush le vector
+			mainShift += insertionIndexesSinceLastJacobsthal.size() * sizeOfElement;
+			insertionIndexesSinceLastJacobsthal.clear();
 
-			// trouve la paire
+			// INFO: trouve la paire
 			int mainBoundInsertion = pendToInsertId + mainShift;
-			// insere et edite lastInsertedMain a l'insertion actuelle
 			std::cout << "mainBoundInsertion = " << mainBoundInsertion << std::endl;
-			lastInsertedMain = placeNumber(main, pend, pendToInsertId, mainBoundInsertion, sizeOfElement);
+
+			// INFO: insere et edite lastInsertedMain a l'insertion actuelle
+			// puis ajoute la position dans le main de l'insertion precedente
+			int insertionIndex = placeNumber(main, pend, pendToInsertId, mainBoundInsertion, sizeOfElement);
+			insertionIndexesSinceLastJacobsthal.push_back(insertionIndex);
 		}
 		prevInsertId = *toInsertId;
 	}
@@ -203,7 +205,7 @@ appendCrumbs
 	main.insert(main.end(), crumbs.begin(), crumbs.end());
 }
 
-// extract incomplete trailing block (miettes) into crumb, trim main to complete blocks only
+// INFO: extract incomplete trailing block (miettes) into crumb, trim main to complete blocks only
 void
 extractCrumbs
 (std::vector<int> &main, std::vector<int> &crumbs, int sizeOfElement, int numberOfElements)
@@ -220,21 +222,18 @@ recursiveSort
 	std::cout << std::left << std::setw(16) << "complete chain" << ": " << printContainer(main, sizeOfElement) << std::endl;
 	int	numberOfElements = main.size() / sizeOfElement;
 
-	// SAFE
-	// extract crumb to not handle it in this recursion level
+	// INFO: extract crumb to not handle it in this recursion level
 	// crumb are numbers that are too few to compose an element
 	std::vector<int> crumbs;
 	extractCrumbs(main, crumbs, sizeOfElement, numberOfElements);
 
-	// SAFE
-	// determine the greater 2 by 2
+	// INFO: determine the greater 2 by 2
 	// then swap the greater right to the smaller
 	sort2By2(main, sizeOfElement);
 
 	std::cout << std::left << std::setw(16) << "after swap" << ": " << printContainer(main, sizeOfElement) << std::endl;
 	
-	// SAFE
-	// if there is less than 3 element,
+	// HACK: if there is less than 3 element,
 	// elements already sorted
 	// ie no pend
 	if (numberOfElements < 3)
@@ -243,31 +242,30 @@ recursiveSort
 		return ;
 	}
 
-	// SAFE
-	// if we can compose at least 2 pair, recursively launch the algoritm
+	// INFO: if we can compose at least 2 pair, recursively launch the algoritm
 	if (numberOfElements >= 4)
 		recursiveSort(main, sizeOfElement * 2);
 
 	std::cout << std::left << std::setw(16) << "after recursion" << ": " << printContainer(main, sizeOfElement) << std::endl;
 	
-	// compose the main chain : all winners and the first loser
+	// INFO: compose the main chain : all winners and the first loser
 	// others are pending elements
 	std::vector<int> pend;
 	extractPend(main, pend, sizeOfElement);
 	std::cout << std::left << std::setw(16) << "Main chain" << ": " << printContainer(main, sizeOfElement) << std::endl;
 	std::cout << std::left << std::setw(16) << "Pending Elements" << ": " << printContainer(pend, sizeOfElement) << std::endl;
 	
-	// UNSAFE BUG COME FROM HERE
-	// insert pending element into main chain:
+	// INFO: insert pending element into main chain:
 		// in the right insertion order
 		// following the bound of each element
-	// to decrease comparison
+		// to decrease comparison
+	// WARN: UNSAFE BUG COME FROM HERE
 	insertPend(main, pend, sizeOfElement);
+
 	std::cout << std::left << std::setw(16) << "main post-insert" << ": " << printContainer(main, sizeOfElement) << std::endl;
 	std::cout << std::left << std::setw(16) << "pend post-insert" << ": " << printContainer(pend, sizeOfElement) << std::endl;
 
-	// SAFE
-	// append crumb that we extracted before to handle it in a lower recursion
+	// INFO: append crumb that we extracted before to handle it in a lower recursion
 	appendCrumbs(main, crumbs);
 }
 
